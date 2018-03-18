2ec166ba5287bbb4d8f104a00241964bf66ab0d0
Eugene Kunitsa <kunitsa.evgeniy@gmail.com>
2018-03-18 22:45:51 +0300

    Add Facebook and Firebase Auth

--- android/app/src/main/java/com/tinderclone/MainApplication.java
+++ android/app/src/main/java/com/tinderclone/MainApplication.java
@@ -3,6 +3,9 @@ package com.tinderclone;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.magus.fblogin.FacebookLoginPackage;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebaseAuthPackage(),
+            new FacebookLoginPackage()
       );
     }
 
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export const fbLoginPermissions = ['public_profile', 'email']
